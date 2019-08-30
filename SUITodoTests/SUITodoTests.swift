2129d34886ccac66bf9772c8510e93dfa5782a77
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-08-30 17:26:24 -0400

    Initial Commit

---
+++ SUITodoTests/SUITodoTests.swift
@@ -0,0 +1,34 @@
+//
+//  SUITodoTests.swift
+//  SUITodoTests
+//
+//  Created by Tommie Carter on 8/30/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import XCTest
+@testable import SUITodo
+
+class SUITodoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
